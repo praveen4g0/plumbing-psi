85547fc495affb4d537f6190c8f163546b02ff67
Pavol Pitonak <ppitonak@redhat.com>
2020-04-22 10:17:30 +0200

    rolebindings for pipelinesdeveloper user

--- config/auth/01-test-auth.sh
+++ config/auth/01-test-auth.sh
@@ -7,3 +7,17 @@ oc create secret generic htpass-secret --from-file=htpasswd=$DIR/users.htpasswd 
 
 echo "Creating a htpasswd identity provider"
 oc apply -f $DIR/test-oauth.yaml
+
+echo "Creating cluster role bindings"
+oc create clusterrolebinding pipelinesdeveloper_basic_user --clusterrole=basic-user --user=pipelinesdeveloper
+oc create clusterrolebinding pipelinesdeveloper_view --clusterrole=view --user=pipelinesdeveloper
+
+echo -e "Use one of the following users to login:\n"
+echo "USERNAME           | PASSWORD    "
+echo "-------------------|-------------"
+echo "user               | user        "
+echo "user1              | user1       "
+echo "...                | ...         "
+echo "user9              | user9       "
+echo "pipelinesdeveloper | developer   "
+echo "---------------------------------"
